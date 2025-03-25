a075ade30e976886641b74bc99b893dc87cfa854
Helma van der Linden <helma.van.der.linden@vzvz.nl>
2025-03-25 13:39:12 +0100

    FIX add final teardown to clean up everything

--- test/bats/setup_suite.bash
+++ test/bats/setup_suite.bash
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Location of the script
+ME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# assume we are in test/bats
+ROOT_DIR="$( dirname "$( dirname "$ME_DIR" )" )"
+
 # Define the path to the script under test
 SCRIPT_UNDER_TEST="${SCRIPT_UNDER_TEST:-./updatePublisher.sh}"
 
@@ -7,4 +13,11 @@ SCRIPT_UNDER_TEST="${SCRIPT_UNDER_TEST:-./updatePublisher.sh}"
 setup_suite() {
   # Export it so tests can use it
   export SCRIPT_UNDER_TEST
+}
+
+teardown_suite() {
+  # clean up once more
+
+  rm -rf ${ROOT_DIR}/tmp/
+  rm -f ${ROOT_DIR}/curl
 }
